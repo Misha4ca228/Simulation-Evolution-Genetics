0edaf0f38e4747b4e4860039a451400e0dfc553f
Misha4ca228 <tvvas1977@gmail.com>
2025-05-12 19:48:44 +0300

    v1.2

--- SEG/utils/color.py
+++ SEG/utils/color.py
@@ -2,5 +2,5 @@ def speed_to_color(speed, max_speed):
     ratio = speed / max_speed
     red = int(255 * ratio)
     blue = int(255 * (1 - ratio))
-    green = 50
+    green = 0
     return (red, green, blue)
